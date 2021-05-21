6117b775e2bf645c601c0f17233b9ac229e82e50
LuoZhiwei <lzw875@foxmail.com>
2021-05-21 23:45:09 +0800

    update

--- biTree.hpp
+++ biTree.hpp
@@ -1,42 +1,79 @@
-template<typename ElemType>
+template <typename ElemType>
 class biTree
 {
 private:
     /* data */
 public:
     ElemType data;
-    biTree<ElemType>* left = nullptr;
-    biTree<ElemType>* right = nullptr;
+    biTree<ElemType> *left = nullptr;
+    biTree<ElemType> *right = nullptr;
     biTree(/* args */);
     ~biTree();
     biTree(const ElemType &e);
-    static void creatBiTree(biTree<ElemType>* root, char *str, int begin, int end);
+    static void creatBiTree(biTree<ElemType> *root, char *str, int *ptr, int end);
+    static void preOrderTraverse(biTree<ElemType> *root);
 };
-template<typename ElemType>
-void biTree<ElemType>::creatBiTree(biTree<ElemType>* root, char *str, int begin, int end)
+/**
+ * @brief 
+ * 
+ * @tparam ElemType 
+ * @param root 
+ */
+template <typename ElemType>
+void biTree<ElemType>::preOrderTraverse(biTree<ElemType> *root)
 {
-    if(str[begin] == '#')
+    if (root == nullptr)
+        return;
+    cout << root->data << endl;
+    preOrderTraverse(root->left);
+    preOrderTraverse(root->right);
+}
+
+/**
+ * @brief 
+ * 
+ * @tparam ElemType 
+ * @param root 
+ * @param str 
+ * @param ptr 
+ * @param end 
+ */
+template <typename ElemType>
+void biTree<ElemType>::creatBiTree(biTree<ElemType> *root, char *str, int *ptr, int end)
+{
+    // base case
+    if (str[*ptr] == '#' || *ptr > end)
     {
+        delete root;
         root = nullptr;
         return;
     }
-    root->data = (ElemType)str[begin];
-    creatBiTree(root->left, str, begin+1, end);
-    // TODO
+
+    ElemType e = (ElemType)str[*ptr];
+    root->data = e;
+    root->left = new biTree<ElemType>();
+    root->right = new biTree<ElemType>();
+    ++*ptr;
+    creatBiTree(root->left, str, ptr, end);
+    ++*ptr;
+    creatBiTree(root->right, str, ptr, end);
 }
 
-template<typename ElemType>
+template <typename ElemType>
 biTree<ElemType>::biTree(const ElemType &e)
 {
     data = e;
 }
 
-template<typename ElemType>
+template <typename ElemType>
 biTree<ElemType>::biTree(/* args */)
 {
 }
 
-template<typename ElemType>
+template <typename ElemType>
 biTree<ElemType>::~biTree()
 {
+    delete this->left;
+    delete this->right;
+    delete &data;
 }
